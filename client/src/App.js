11d5838264ab0f22a4c2a711b4d735a7c0a1c119
EMMANUEL BROWN <emmanuelbrown1310@gmail.com>
2020-08-28 18:53:22 -0400

    start pack

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  //Redirect
+} from 'react-router-dom'
+
+import "./App.css"
+
+import Navbar from './components/navbar';
+
+//Pages
+import Home from './pages/Home/';
+
+function App() {
+  return (
+    <div id="cover">
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
